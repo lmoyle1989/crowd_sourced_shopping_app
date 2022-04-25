79001157aa7a7c2806e8fa5545fe314e89e476cb
Marodin Shahbaz Yourghanlo <shahbazm@oregonstate.edu>
2022-04-24 23:35:54 -0700

    postgres uri change for deployment

--- server/crowdshop_api/config/config.py
+++ server/crowdshop_api/config/config.py
@@ -4,12 +4,12 @@ import os
 class MainConfig(object):
     SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
     JWT_SECRET = os.getenv('JWT_SECRET', 'testing')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 
 class Development(MainConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 
 class Production(MainConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'SQLALCHEMY_DATABASE_URI').replace('postgres://', 'postgresql://')
