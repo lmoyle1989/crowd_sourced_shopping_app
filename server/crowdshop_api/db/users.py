033f6240af1598a0a85530b0fe28c51678890d7e
Marodin Shahbaz Yourghanlo <shahbazm@oregonstate.edu>
2022-05-10 20:31:39 -0700

    users name non-unique

--- server/crowdshop_api/db/users.py
+++ server/crowdshop_api/db/users.py
@@ -4,8 +4,8 @@ from db import db
 class Users(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    first_name = db.Column(db.String(60), unique=True, nullable=False)
-    last_name = db.Column(db.String(60), unique=True, nullable=False)
+    first_name = db.Column(db.String(60), unique=False, nullable=False)
+    last_name = db.Column(db.String(60), unique=False, nullable=False)
     password = db.Column(db.String(32), nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
     uploads_count = db.Column(db.Integer)
