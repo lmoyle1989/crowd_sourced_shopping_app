00ca00f179dc9a810ca81adbf0a5ffba1ea42cd4
Ciarra Murphy <mciarra@ymail.com>
2022-05-23 15:56:53 -0500

    added user comment table code

---
+++ server/crowdshop_api/db/user_comments.py
@@ -0,0 +1,17 @@
+from db import db
+from db.users import Users
+
+class UserComments(db.Model):
+    """Creates a table to hold comments for Live Feed page and connects to user id in Users table"""
+    comment_id = db.column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    comment = db.column(db.String(500), nullable=False)
+    date = db.column(db.DATETIME, nullable=False)
+
+    comment_relationship = db.relationship('Users', backref='user_comments', lazy = True)
+
+    def __init__(self, user_id, comment, date):
+        self.user_id = user_id
+        self.comment = comment
+        self.date = date
+        
