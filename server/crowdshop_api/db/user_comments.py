655dd7f2a7071f42e64d6e09c24abad248309a45
Ciarra Murphy <mciarra@ymail.com>
2022-05-25 16:20:37 -0500

    added routes code for comments table

--- server/crowdshop_api/db/user_comments.py
+++ server/crowdshop_api/db/user_comments.py
@@ -4,14 +4,13 @@ from db.users import Users
 class UserComments(db.Model):
     """Creates a table to hold comments for Live Feed page and connects to user id in Users table"""
     comment_id = db.column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    comment_user_id = db.column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     comment = db.column(db.String(500), nullable=False)
     date = db.column(db.DATETIME, nullable=False)
 
     comment_relationship = db.relationship('Users', backref='user_comments', lazy = True)
 
-    def __init__(self, user_id, comment, date):
-        self.user_id = user_id
+    def __init__(self, comment_user_id, comment, date):
+        self.comment_user_id = comment_user_id
         self.comment = comment
         self.date = date
-        
